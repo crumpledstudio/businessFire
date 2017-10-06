bd78591865bb5062768afb4bdc64326bbf54459c
Noxyo <sgodondrugs@gmail.com>
2017-10-06 04:50:58 +0100

    Todo implementation Try #1

--- businesscontacts/src/app/app.component.ts
+++ businesscontacts/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 
-//import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
 //import { AngularFireModule } from 'angularfire2';
 //import { AngularFireDatabase } from 'angularfire2/database';
 //import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument  } from 'angularfire2/firestore';
+
+export interface Todo {
+  id?: string;
+  description: string;
+  completed: boolean;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -13,9 +21,49 @@ import { AngularFirestore } from 'angularfire2/firestore';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  /*
   title = 'app';
   items: Observable<any[]>;
+
   constructor(db: AngularFirestore) {
     this.items = db.collection('items').valueChanges();
   }
+  
+  ngOnInit() {
+
+  }
+  */
+  todoCollectionRef: AngularFirestoreCollection<Todo>;
+  todos: Observable<Todo[]>;
+  constructor(private afs: AngularFirestore){
+    this.todoCollectionRef = this.afs.collection<Todo>('todos');
+    //this.todos = this.todoCollectionRef.valueChanges();
+    this.todos = this.todoCollectionRef.snapshotChanges().map(actions => {
+      return actions.map(action => {
+        const data = action.payload.doc.data() as Todo;
+        const id = action.payload.doc.id;
+        return { id, ...data };
+      });
+    });
+  }
+
+  // Add Todo
+  addTodo(todoDesc: string) {
+    if (todoDesc && todoDesc.trim().length) {
+      this.todoCollectionRef.add({ description: todoDesc, completed: false });
+    }
+  }
+
+  // Update Todo
+  updateTodo(todo: Todo) {
+    this.todoCollectionRef.doc(todo.id).update({ completed: !todo.completed });
+  }
+
+  // Delete Todo
+  deleteTodo(todo: Todo) {
+    this.todoCollectionRef.doc(todo.id).delete();
+  }
+
+
 }
--- businesscontacts/src/app/app.module.ts
+++ businesscontacts/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { FirebaseService } from './services/firebase.service';
 
 // Firebase configuration
-import { AngularFireModule } from 'angularfire2';
 //import { AngularFireDatabaseModule } from 'angularfire2/database';
 //import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
 
 @NgModule({
   declarations: [
